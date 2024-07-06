4a365cce8cade0ebbc54c8e0d994ffbf5b1153ee
bosio904 <anguema@cibernos.com>
2024-07-06 13:01:58 +0200

    nuevo

--- saludo.py
+++ saludo.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-def saludar(nombre):
+
+def saludando(nombre):
     print(f"Hola, {nombre}!")
 
 if __name__ == "__main__":
     nombre = input("Introduce tu nombre: ")
-=======
-def saludar(nombre):
-    print(f"Hola, {nombre}!")
-
-if __name__ == "__main__":
-    nombre = input("Introduce tu nombre: ")
->>>>>>> 56ab022cd925e65472ed7bf822d39f0e6306b09b
-    saludar(nombre)
